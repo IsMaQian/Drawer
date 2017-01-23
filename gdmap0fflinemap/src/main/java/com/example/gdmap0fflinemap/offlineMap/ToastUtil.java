b6930e378ecd4ba837b7b37938b7720591d665a9
IsMaQian <252919181@qq.com>
2017-01-23 11:46:39 +0800

    Initial commit

---
+++ gdmap0fflinemap/src/main/java/com/example/gdmap0fflinemap/offlineMap/ToastUtil.java
@@ -0,0 +1,50 @@
+package com.example.gdmap0fflinemap.offlineMap;
+
+/**
+ * Created by Administrator on 2017/1/19.
+ */
+
+import android.content.Context;
+import android.os.Handler;
+import android.view.Gravity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.gdmap0fflinemap.R;
+
+/**
+ *
+ */
+public class ToastUtil {
+
+    private static Toast mToast;
+
+    private static Handler mHandler = new Handler();
+    private static Runnable r = new Runnable() {
+        public void run() {
+            if(mToast != null) {
+                mToast.cancel();
+                mToast = null;// toast隐藏后，将其置为null
+            }
+        }
+    };
+
+    public static void showShortToast(Context context, String message) {
+        TextView text = new TextView(context);// 显示的提示文字
+        text.setText(message);
+        text.setBackgroundColor(context.getResources().getColor(R.color.transparent));
+        text.setPadding(10, 10, 10, 10);
+
+        if (mToast != null) {//
+            mHandler.postDelayed(r, 0);//隐藏toast
+        } else {
+            mToast = new Toast(context);
+            mToast.setDuration(Toast.LENGTH_SHORT);
+            mToast.setGravity(Gravity.BOTTOM, 0, 150);
+            mToast.setView(text);
+        }
+
+        mHandler.postDelayed(r, 1000);// 延迟1秒隐藏toast
+        mToast.show();
+    }
+}
